9a2f1ff92f2683b685680908a9e072e082120eb4
lucienlmy <101241027+lucienlmy@users.noreply.github.com>
2024-11-16 22:21:38 +0800

    123333

---
+++ GUI/SIDScaleform.cs
@@ -0,0 +1,47 @@
+﻿using FusionLibrary;
+using GTA.UI;
+using System;
+using System.Drawing;
+
+namespace BackToTheFutureV
+{
+    internal class SIDScaleform : ScaleformGui
+    {
+        private PropertiesHandler Properties => TimeMachineHandler.ClosestTimeMachine.Properties;
+
+        private static PointF SID3DLocation = new PointF() { X = 0.626f, Y = 0.626f };
+        private static readonly float SID3DScale = 1.284f;
+
+        public SIDScaleform(bool is2D) : base(is2D ? "bttf_2d_sid" : "bttf_3d_sid")
+        {
+
+        }
+
+        private void SetLed()
+        {
+            if (TcdEditer.IsEditing)
+            {
+                return;
+            }
+
+            for (int column = 0; column < 10; column++)
+            {
+                CallFunction("setLed", column, Convert.ToInt32(Properties.CurrentHeight[column]));
+            }
+        }
+
+        public void Draw2D()
+        {
+            CallFunction("setBackground", 2);
+
+            SetLed();
+            Render2D(ModSettings.SIDPosition, new SizeF(ModSettings.SIDScale * (800f / 1414f) / Screen.AspectRatio, ModSettings.SIDScale));
+        }
+
+        public void Draw3D()
+        {
+            SetLed();
+            Render2D(SID3DLocation, new SizeF(SID3DScale * (800f / 1414f) / Screen.AspectRatio, SID3DScale));
+        }
+    }
+}
---
+++ GUI/TCD2DScaleform.cs
@@ -0,0 +1,153 @@
+﻿using FusionLibrary;
+using GTA.UI;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using static BackToTheFutureV.InternalEnums;
+
+namespace BackToTheFutureV
+{
+    internal class TCD2DScaleform : ScaleformGui
+    {
+        public TCD2DScaleform() : base("bttf_2d_gui")
+        {
+            dates["red"] = new DateTime();
+            dates["green"] = new DateTime();
+            dates["yellow"] = new DateTime();
+        }
+
+        private readonly Dictionary<string, DateTime> dates = new Dictionary<string, DateTime>();
+
+        public void Draw2D()
+        {
+            Render2D(ModSettings.TCDPosition, new SizeF(ModSettings.TCDScale * (1501f / 1100f) / Screen.AspectRatio, ModSettings.TCDScale));
+        }
+
+        public void SetEmpty(EmptyType emptyType)
+        {
+            switch (emptyType)
+            {
+                case EmptyType.Off:
+                    CallFunction("SET_EMPTY_STATE", 0);
+                    break;
+                case EmptyType.On:
+                    CallFunction("SET_EMPTY_STATE", 1);
+                    break;
+                default:
+                    CallFunction("HIDE_EMPTY");
+                    break;
+            }
+        }
+
+        public void SetDate(string type, DateTime date)
+        {
+            dates[type] = date;
+
+            CallFunction("SET_" + type.ToUpper() + "_MONTH", date.Month);
+            CallFunction("SET_" + type.ToUpper() + "_DAY", date.Day);
+            CallFunction("SET_" + type.ToUpper() + "_YEAR", date.Year);
+            CallFunction("SET_" + type.ToUpper() + "_HOUR", ((date.Hour + 11) % 12) + 1);
+            CallFunction("SET_" + type.ToUpper() + "_MINUTE", date.Minute);
+
+            CallFunction("SET_AM_PM", type.ToLower(), date.ToString("tt", CultureInfo.InvariantCulture) == "AM" ? 1 : 2);
+        }
+
+        public void SetSpeedoBackground(bool state)
+        {
+            CallFunction("SET_SPEEDO_BACKGROUND", Convert.ToInt32(state) + 1);
+        }
+
+        public void SetBackground(TCDBackground background)
+        {
+            CallFunction("SET_TCD_BACKGROUND", GetStringFromBackgroundType(background));
+        }
+
+        public void SetDiodeState(bool state)
+        {
+            CallFunction("SET_DIODE_STATE", state);
+        }
+
+        public void SetVisible(string type, bool toggle, bool month = true, bool day = true, bool year = true, bool hour = true, bool minute = true, bool amPm = true)
+        {
+            if (toggle)
+            {
+                SetDate(type, dates[type]);
+
+                if (!month)
+                {
+                    CallFunction("SET_" + type.ToUpper() + "_MONTH", -1);
+                }
+
+                if (!day)
+                {
+                    CallFunction("SET_" + type.ToUpper() + "_DAY", -1);
+                }
+
+                if (!year)
+                {
+                    CallFunction("SET_" + type.ToUpper() + "_YEAR", -1);
+                }
+
+                if (!hour)
+                {
+                    CallFunction("SET_" + type.ToUpper() + "_HOUR", -1);
+                }
+
+                if (!minute)
+                {
+                    CallFunction("SET_" + type.ToUpper() + "_MINUTE", -1);
+                }
+
+                if (!amPm)
+                {
+                    CallFunction("SET_AM_PM", type.ToLower(), 3);
+                }
+            }
+            else
+            {
+                if (month)
+                {
+                    CallFunction("SET_" + type.ToUpper() + "_MONTH", -1);
+                }
+
+                if (day)
+                {
+                    CallFunction("SET_" + type.ToUpper() + "_DAY", -1);
+                }
+
+                if (year)
+                {
+                    CallFunction("SET_" + type.ToUpper() + "_YEAR", -1);
+                }
+
+                if (hour)
+                {
+                    CallFunction("SET_" + type.ToUpper() + "_HOUR", -1);
+                }
+
+                if (minute)
+                {
+                    CallFunction("SET_" + type.ToUpper() + "_MINUTE", -1);
+                }
+
+                if (amPm)
+                {
+                    CallFunction("SET_AM_PM", type.ToLower(), 3);
+                }
+            }
+        }
+
+        private string GetStringFromBackgroundType(TCDBackground background)
+        {
+            if (background == TCDBackground.Metal)
+            {
+                return "metal";
+            }
+            else
+            {
+                return "trans";
+            }
+        }
+    }
+}
